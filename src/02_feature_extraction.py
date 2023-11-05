42f2f9056f123c3c16e9d09a1ef461bd8a7e0f7c
Haroon <haroonr1@umbc.edu>
2023-11-05 14:41:24 -0500

    add code for feature extraction and matching

--- src/02_feature_extraction.py
+++ src/02_feature_extraction.py
@@ -1,3 +1,39 @@
 # Feature extraction code
 # Implement feature extraction here
 # Import necessary libraries (e.g., OpenCV, scikit-image)
+import cv2
+import numpy as np
+import os
+
+
+# Set the working directory to the project root
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(project_root)
+
+# List of image paths
+image_paths = ['data/images/image0.jpg', 'data/images/image1.jpg', 'data/images/image2.jpg', 'data/images/image3.jpg']
+
+# Initialize the SIFT feature extractor
+sift = cv2.SIFT_create()
+
+keypoints_list = []  # List to store keypoints for each image
+descriptors_list = []  # List to store descriptors for each image
+
+# Iterate through the images and perform feature extraction
+for image_path in image_paths:
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+    # Detect keypoints and compute descriptors
+    keypoints, descriptors = sift.detectAndCompute(image, None)
+    
+    # Convert keypoints to a list of (x, y, size, angle, response, octave, class_id)
+    keypoints_data = [(kp.pt[0], kp.pt[1], kp.size, kp.angle, kp.response, kp.octave, kp.class_id) for kp in keypoints]
+    
+    keypoints_list.append(keypoints_data)
+    descriptors_list.append(descriptors)
+
+# Save the keypoints as a list of lists
+for i in range(len(image_paths)):
+    np.save(f'data/keypoints_image{i}.npy', keypoints_list[i])
+    np.save(f'data/descriptors_image{i}.npy', descriptors_list[i])
+
--- src/03_feature_matching.py
+++ src/03_feature_matching.py
@@ -1,3 +1,46 @@
-# Feature matching code
-# Implement feature matching and correspondence filtering here
-# Import necessary libraries (e.g., OpenCV)
+import cv2
+import numpy as np
+import os
+
+# Set the working directory to the project root
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(project_root)
+
+image_paths = ['data/images/image0.jpg', 'data/images/image1.jpg', 'data/images/image2.jpg', 'data/images/image3.jpg']
+
+# Load keypoints and descriptors for the images
+keypoints_file_paths = ['data/keypoints_image0.npy', 'data/keypoints_image1.npy', 'data/keypoints_image2.npy', 'data/keypoints_image3.npy']
+descriptors_file_paths = ['data/descriptors_image0.npy', 'data/descriptors_image1.npy', 'data/descriptors_image2.npy', 'data/descriptors_image3.npy']
+
+# Initialize the feature matcher (in this case, Brute-Force matcher)
+bf = cv2.BFMatcher()
+
+# Match descriptors between pairs of images
+matches = []  # List to store matches for each pair of images
+
+# Load keypoints for each image
+keypoints_list = []
+for kp_file_path in keypoints_file_paths:
+    keypoints = np.load(kp_file_path)
+    keypoints_list.append(keypoints)
+
+for i in range(len(keypoints_file_paths) - 1):
+    descriptors1 = np.load(descriptors_file_paths[i])
+    descriptors2 = np.load(descriptors_file_paths[i + 1])
+    
+    # Perform brute-force matching
+    matches_i = bf.knnMatch(descriptors1, descriptors2, k=2)
+    
+    # Apply ratio test to get good matches
+    good_matches = []
+    for m, n in matches_i:
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+    
+    matches.append(good_matches)
+
+# Save the matches for each pair of images (optional)
+for i, match_list in enumerate(matches):
+    matches_to_save = [{'queryIdx': m.queryIdx, 'trainIdx': m.trainIdx, 'distance': m.distance} for m in match_list]
+    np.save(f'data/matches_image{i}_to_image{i + 1}.npy', matches_to_save)
+
