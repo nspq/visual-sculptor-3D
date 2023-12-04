c0e0e42fb3c6ebec09fe54d46ef1acfb439a3352
nspq <nblsyed@hotmail.com>
2023-12-04 12:50:18 -0500

    Visualize matches

--- src/03_feature_matching.py
+++ src/03_feature_matching.py
@@ -11,6 +11,32 @@ def match_features(descriptors1, descriptors2, bf, ratio_threshold=0.75):
 
     return good_matches
 
+def display_matches(img1, img2, keypoints1, keypoints2, matches, i1, i2):
+    # Create a new image with double the width and height of the larger image
+    height = max(img1.shape[0], img2.shape[0]) * 2
+    width = max(img1.shape[1], img2.shape[1]) * 2
+    new_image = np.zeros((height, width, 3), dtype=np.uint8)
+
+    # Place the two images side by side on the new image
+    new_image[:img1.shape[0], :img1.shape[1]] = img1
+    new_image[:img2.shape[0], img1.shape[1]:] = img2
+
+    # Draw lines connecting the matches
+    # Draw lines connecting the matches
+    for match in matches:
+        img1_idx = match.queryIdx
+        img2_idx = match.trainIdx
+
+        # Coordinates in the larger combined image
+        pt1 = (int(keypoints1[img1_idx][0]), int(keypoints1[img1_idx][1]))
+        pt2 = (int(keypoints2[img2_idx][0] + img1.shape[1]), int(keypoints2[img2_idx][1]))
+
+        cv2.line(new_image, pt1, pt2, (0, 255, 0), 8)
+
+
+    # Save the resulting image
+    cv2.imwrite(f'outputs/imageMatch{i1}and{i2}.jpg', new_image)
+
 def main():
     # Set the working directory to the project root
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +65,11 @@ def main():
         good_matches = match_features(descriptors1, descriptors2, bf)
 
         matches.append(good_matches)
+
+        # Display and save the matches
+        img1 = cv2.imread(image_paths[i])
+        img2 = cv2.imread(image_paths[i + 1])
+        display_matches(img1, img2, keypoints_list[i], keypoints_list[i + 1], good_matches, i, i+1)
 
     # Save the matches for each pair of images (optional)
     for i, match_list in enumerate(matches):
