1235407eddff525c9e21075b5b6fcf048f15c2a5
Haroon <haroonr1@umbc.edu>
2023-12-05 12:54:54 -0500

    make code scalable

--- src/02_feature_extraction.py
+++ src/02_feature_extraction.py
@@ -1,57 +1,69 @@
+# Usage:
+# python3 02_feature_extraction.py src/data
+
+
 # Feature extraction code
 # Implement feature extraction here
 # Import necessary libraries (e.g., OpenCV, scikit-image)
 import cv2
 import numpy as np
 import os
+import sys
+
+def extract_features(image_dir):
+    # Set the working directory to the project root
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(project_root)
+
+    # Create output directories if they don't exist
+    keypoints_dir = os.path.join(image_dir, 'keypoints')
+    descriptors_dir = os.path.join(image_dir, 'descriptors')
+    outputs_dir = os.path.join(image_dir, 'outputs')
+
+    os.makedirs(keypoints_dir, exist_ok=True)
+    os.makedirs(descriptors_dir, exist_ok=True)
+    os.makedirs(outputs_dir, exist_ok=True)
 
 
-# Set the working directory to the project root
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.chdir(project_root)
+    # List image files in the specified directory
+    image_files = [f for f in os.listdir(image_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    image_paths = [os.path.join(image_dir, f) for f in image_files]
 
-# List of image paths
-image_paths = ['data/images/image0.jpg', 'data/images/image1.jpg', 'data/images/image2.jpg', 'data/images/image3.jpg']
+    # Initialize the SIFT feature extractor
+    sift = cv2.SIFT_create()
 
-# Initialize the SIFT feature extractor
-sift = cv2.SIFT_create()
+    keypoints_list = []  # List to store keypoints for each image
+    descriptors_list = []  # List to store descriptors for each image
 
-keypoints_list = []  # List to store keypoints for each image
-descriptors_list = []  # List to store descriptors for each image
+    # Iterate through the images and perform feature extraction
+    for image_path in image_paths:
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Iterate through the images and perform feature extraction
-for image_path in image_paths:
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        # Detect keypoints and compute descriptors
+        keypoints, descriptors = sift.detectAndCompute(image, None)
 
-    # Get the size of the loaded image
-    gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    image_size = (gray_image.shape[1], gray_image.shape[0])  # (width, height)
+        # Draw keypoints on the black image
+        image_with_keypoints = cv2.drawKeypoints(image, keypoints, outImage=None)
 
-    # Create a black image with the same size as canvas
-    blank_image = np.zeros((image_size[1], image_size[0], 3), dtype=np.uint8)
-    
-    # Detect keypoints and compute descriptors
-    keypoints, descriptors = sift.detectAndCompute(image, None)
-    
-    # Draw keypoints on the black image
-    image_with_keypoints = cv2.drawKeypoints(blank_image, keypoints, outImage=None)
-    
-    # Show the image with keypoints
-    fname = f"outputs/imgOutputVis{image_path[17]}.jpg"
+        # Show the image with keypoints
+        fname = os.path.join(outputs_dir, f"imgOutputVis{os.path.splitext(os.path.basename(image_path))[0]}.jpg")
 
-    cv2.imwrite(fname, image_with_keypoints)
-    #cv2.imshow("Keypoints of Image", image_with_keypoints)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    
-    # Convert keypoints to a list of (x, y, size, angle, response, octave, class_id)
-    keypoints_data = [(kp.pt[0], kp.pt[1], kp.size, kp.angle, kp.response, kp.octave, kp.class_id) for kp in keypoints]
-    
-    keypoints_list.append(keypoints_data)
-    descriptors_list.append(descriptors)
+        cv2.imwrite(fname, image_with_keypoints)
 
-# Save the keypoints as a list of lists
-for i in range(len(image_paths)):
-    np.save(f'data/keypoints_image{i}.npy', keypoints_list[i])
-    np.save(f'data/descriptors_image{i}.npy', descriptors_list[i])
+        # Convert keypoints to a list of (x, y, size, angle, response, octave, class_id)
+        keypoints_data = [(kp.pt[0], kp.pt[1], kp.size, kp.angle, kp.response, kp.octave, kp.class_id) for kp in keypoints]
 
+        keypoints_list.append(keypoints_data)
+        descriptors_list.append(descriptors)
+
+    # Save the keypoints as a list of lists
+    for i in range(len(image_files)):
+        np.save(os.path.join(keypoints_dir, f'keypoints_image{i}.npy'), keypoints_list[i])
+        np.save(os.path.join(descriptors_dir, f'descriptors_image{i}.npy'), descriptors_list[i])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 02_feature_extraction.py /path/to/image/directory")
+    else:
+        image_directory = sys.argv[1]
+        extract_features(image_directory)
--- src/03_feature_matching.py
+++ src/03_feature_matching.py
@@ -11,7 +11,7 @@ def match_features(descriptors1, descriptors2, bf, ratio_threshold=0.75):
 
     return good_matches
 
-def display_matches(img1, img2, keypoints1, keypoints2, matches, i1, i2):
+def display_matches(img1, img2, keypoints1, keypoints2, matches, i1, i2, outputs_dir):
     # Create a new image with double the width and height of the larger image
     height = max(img1.shape[0], img2.shape[0]) * 2
     width = max(img1.shape[1], img2.shape[1]) * 2
@@ -21,7 +21,6 @@ def display_matches(img1, img2, keypoints1, keypoints2, matches, i1, i2):
     new_image[:img1.shape[0], :img1.shape[1]] = img1
     new_image[:img2.shape[0], img1.shape[1]:] = img2
 
-    # Draw lines connecting the matches
     # Draw lines connecting the matches
     for match in matches:
         img1_idx = match.queryIdx
@@ -33,26 +32,43 @@ def display_matches(img1, img2, keypoints1, keypoints2, matches, i1, i2):
 
         cv2.line(new_image, pt1, pt2, (0, 255, 0), 8)
 
-
     # Save the resulting image
-    cv2.imwrite(f'outputs/imageMatch{i1}and{i2}.jpg', new_image)
+    fname = os.path.join(outputs_dir, f"imageMatch{i1}and{i2}.jpg")
+    cv2.imwrite(fname, new_image)
 
 def main():
     # Set the working directory to the project root
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     os.chdir(project_root)
 
-    image_paths = ['data/images/image0.jpg', 'data/images/image1.jpg', 'data/images/image2.jpg', 'data/images/image3.jpg']
+    data_dir = os.path.join(project_root, 'src', 'data')
 
-    # Load keypoints and descriptors for the images
-    keypoints_file_paths = ['data/keypoints_image0.npy', 'data/keypoints_image1.npy', 'data/keypoints_image2.npy', 'data/keypoints_image3.npy']
-    descriptors_file_paths = ['data/descriptors_image0.npy', 'data/descriptors_image1.npy', 'data/descriptors_image2.npy', 'data/descriptors_image3.npy']
+    # Dynamically get image paths
+    image_files = [f for f in os.listdir(data_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    image_paths = [os.path.join(data_dir, f) for f in image_files]
+    
+    keypoints_dir = os.path.join(data_dir, "keypoints")
+    descriptors_dir = os.path.join(data_dir, "descriptors")
+    matches_dir = os.path.join(data_dir, "matches")
+    outputs_dir = os.path.join(data_dir, "outputs")
+
+    # Ensure that the matches directory exists or create it
+    os.makedirs(matches_dir, exist_ok=True)
+
+    # Dynamically get keypoints and descriptors file paths
+    keypoints_file_paths = [os.path.join(keypoints_dir, f) for f in os.listdir(keypoints_dir) if f.startswith('keypoints')]
+    descriptors_file_paths = [os.path.join(descriptors_dir, f) for f in os.listdir(descriptors_dir) if f.startswith('descriptors')]
+
+    # Sort the file paths for consistency
+    image_paths.sort()
+    keypoints_file_paths.sort()
+    descriptors_file_paths.sort()
 
     # Initialize the feature matcher (in this case, Brute-Force matcher)
     bf = cv2.BFMatcher()
 
     # Match descriptors between pairs of images
-    matches = []  # List to store matches for each pair of images
+    matches_list = []  # List to store matches for each pair of images
 
     # Load keypoints for each image
     keypoints_list = [np.load(kp_file_path) for kp_file_path in keypoints_file_paths]
@@ -64,17 +80,37 @@ def main():
         # Perform feature matching
         good_matches = match_features(descriptors1, descriptors2, bf)
 
-        matches.append(good_matches)
+        # Implement RANSAC to estimate homography
+        src_pts = np.float32([keypoints_list[i][m.queryIdx][:2] for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([keypoints_list[i + 1][m.trainIdx][:2] for m in good_matches]).reshape(-1, 1, 2)
 
-        # Display and save the matches
+        # Use RANSAC to estimate homography
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        # Apply the estimated homography to the first image
         img1 = cv2.imread(image_paths[i])
-        img2 = cv2.imread(image_paths[i + 1])
-        display_matches(img1, img2, keypoints_list[i], keypoints_list[i + 1], good_matches, i, i+1)
+        warped_img1 = cv2.warpPerspective(img1, M, (img1.shape[1] + img1.shape[1], img1.shape[0]))
 
-    # Save the matches for each pair of images (optional)
-    for i, match_list in enumerate(matches):
-        matches_to_save = [{'queryIdx': m.queryIdx, 'trainIdx': m.trainIdx, 'distance': m.distance} for m in match_list]
-        np.save(f'data/matches_image{i}_to_image{i + 1}.npy', matches_to_save)
+        # Resize the warped image to match the size of the second image
+        warped_img1 = cv2.resize(warped_img1, (cv2.imread(image_paths[i + 1]).shape[1], cv2.imread(image_paths[i + 1]).shape[0]))
+
+        # Concatenate the warped image and the second image
+        result = np.concatenate([warped_img1, cv2.imread(image_paths[i + 1])], axis=1)
+
+        # Save the resulting image
+        fname = os.path.join(outputs_dir, f"imageMatchRANSAC{i}and{i + 1}.jpg")
+        cv2.imwrite(fname, result)
+
+        # Convert cv2.DMatch objects to dictionaries
+        matches_to_save = [{'queryIdx': m.queryIdx, 'trainIdx': m.trainIdx, 'distance': m.distance} for m in good_matches]
+
+        # Save the RANSAC matches as an .npy file
+        np.save(os.path.join(matches_dir, f'matches_image{i}_to_image{i + 1}.npy'), matches_to_save)
+
+        # Call display_matches function to draw and save matches
+        display_matches(img1, cv2.imread(image_paths[i + 1]), keypoints_list[i], keypoints_list[i + 1], good_matches, i, i + 1, outputs_dir)
+
+
 
 if __name__ == "__main__":
     main()
